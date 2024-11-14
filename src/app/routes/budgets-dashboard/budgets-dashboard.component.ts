3fe605431761b56d5d1b81f17334ed6a23c72d0c
AlisonTDoyle <s00233102@atu.ie>
2024-11-14 20:56:12 +0000

    :sparkles: Set up basic routes

---
+++ src/app/routes/budgets-dashboard/budgets-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-budgets-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './budgets-dashboard.component.html',
+  styleUrl: './budgets-dashboard.component.scss'
+})
+export class BudgetsDashboardComponent {
+
+}
