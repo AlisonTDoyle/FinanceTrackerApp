dfeda4be05971bf62881a61b7bd2dc4ecf098444
AlisonDoyle-SleaterSolutions <adoyle@sleatersolutions.com>
2025-03-28 23:23:10 +0000

    add unit test

--- src/app/services/finance-tracker-api/finance-tracker-api.service.spec.ts
+++ src/app/services/finance-tracker-api/finance-tracker-api.service.spec.ts
@@ -1,16 +1,58 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FinanceTrackerApiService } from './finance-tracker-api.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Transaction } from '../../interfaces/transaction';
 
 describe('FinanceTrackerApiService', () => {
   let service: FinanceTrackerApiService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(FinanceTrackerApiService);
-  });
+    describe('FinanceTrackerApiService', () => {
+      let service: FinanceTrackerApiService;
+      let httpMock: HttpTestingController;
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+      beforeEach(() => {
+        TestBed.configureTestingModule({
+          imports: [HttpClientTestingModule],
+          providers: [FinanceTrackerApiService]
+        });
+        service = TestBed.inject(FinanceTrackerApiService);
+        httpMock = TestBed.inject(HttpTestingController);
+      });
+
+      afterEach(() => {
+        httpMock.verify();
+      });
+
+      it('should be created', () => {
+        expect(service).toBeTruthy();
+      });
+
+      it('should create a transaction', () => {
+        const mockTransaction: Transaction = {
+          _id: '1',
+          price: 100,
+          description: 'Test transaction',
+          date: new Date(),
+          name: "test",
+          category: {
+            user: "456",
+            name: "category"
+          },
+          user: '456'
+        };
+
+        service.CreateTransaction(mockTransaction).subscribe((response) => {
+          expect(response).toEqual(mockTransaction);
+        });
+
+        const req = httpMock.expectOne(service['_transactionUrl']);
+        expect(req.request.method).toBe('POST');
+        expect(req.request.body).toEqual(mockTransaction);
+        req.flush(mockTransaction);
+      });
+    });
   });
 });
